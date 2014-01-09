121f06663783d1c30428b4789a7e8f5d83b2ff5a
barbayellow <gregoire@byl.fr>
2014-01-09 15:43:24 +0100

    path to database.class.php correction

--- view.php
+++ view.php
@@ -1,5 +1,5 @@
 <?php
-require_once('database.class.php');
+require_once('lib/database.class.php');
 
 // Create db object
 $dbcon = new database();
@@ -14,7 +14,7 @@ $data = $dbcon->queryArrayFetchAll();
 	<title>Députés AN</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/bootstrap.min.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
